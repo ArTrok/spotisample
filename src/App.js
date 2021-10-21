26e673392d446764eb42914fdd659e72ca9efbb6
Arthur <arthurteixeira0805@gmail.com>
2021-10-21 16:27:07 -0300

    requirement 2 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
+import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 
 class App extends React.Component {
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,71 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
-  render() {
-    return (
-      <div data-testid="page-login">
-        {null}
-      </div>
-    );
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      email: '',
+      image: '',
+      description: '',
+      loadState: false,
+      redirectToSearch: false,
+    };
   }
+
+    change = ({ target: { name, value } }) => {
+      this.setState({
+        [name]: value,
+      });
+    };
+
+    buttonClick = async () => {
+      const { name, email, image, description } = this.state;
+      this.setState({ loadState: true },
+        () => createUser({ name, email, image, description })
+          .then(() => this.setState({
+            loadState: false,
+            redirectToSearch: true,
+          })));
+    };
+
+    render() {
+      const { name, loadState, redirectToSearch } = this.state;
+      const REQ_L = 3;
+      if (redirectToSearch) return <Redirect to="/search" />;
+      return (
+        <div data-testid="page-login">
+          {loadState ? (
+            <Loading />
+          ) : (
+            <form>
+              <label htmlFor="name">
+                Name
+                <input
+                  data-testid="login-name-input"
+                  type="text"
+                  name="name"
+                  onChange={ this.change }
+                  value={ name }
+                />
+              </label>
+              <button
+                data-testid="login-submit-button"
+                type="button"
+                disabled={ name.length < REQ_L }
+                onClick={ this.buttonClick }
+              >
+                Entrar
+              </button>
+            </form>
+          )}
+        </div>
+      );
+    }
 }
 
 export default Login;
