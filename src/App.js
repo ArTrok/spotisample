77cfabac8dc8d7e487deffe6757ff0c6af390779
Arthur <arthurteixeira0805@gmail.com>
2021-10-23 20:10:36 -0300

    correction

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
-import ProfileEdit from './pages/ProfileEdit';
 import Profile from './pages/Profile';
-import Login from './pages/Login';
+import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={ Login } />
-        <Route path="/search" component={ Search } />
-        <Route path="/album/:id" component={ Album } />
-        <Route path="/favorites" component={ Favorites } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route path="*" component={ NotFound } />
+        <Switch>
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/search" component={ Search } />
+          <Route exact path="/" component={ Login } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
       </BrowserRouter>
     );
   }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
+import searchAlbumsAPI from '../services/userAPI';
 import Header from './Header';
 
 class Search extends React.Component {
-  // constructor() {
-  //   super();
+  constructor() {
+    super();
 
-  //   // this.state = {
-  //   //   artistName: '',
-  //   //   loading: false,
-  //   //   albums: [],
-  //   //   artist: '',
-  //   //   foundAlbums: false,
-  //   // };
-  // }
+    this.state = {
+      artName: '',
+      loadState: false,
+      currAlbums: [],
+      artistKept: '',
+      albumsExists: false,
+    };
+  }
+
+  change = ({ target: { value } }) => {
+    this.setState({ artName: value });
+  };
+
+  searchButton = async () => {
+    const { artName } = this.state;
+    const albumsFetch = await searchAlbumsAPI(artName);
+    this.setState({ loadState: true },
+      albumsFetch()
+        .then(() => this.setState({
+          loadState: false,
+          artName: '',
+          artistKept: artName,
+          currAlbums: albumsFetch,
+          albumsExists: true,
+        })));
+  }
 
   render() {
     return (
