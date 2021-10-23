f60730429602d82eb163c3c797a2d96cfac2a5d3
Arthur <arthurteixeira0805@gmail.com>
2021-10-23 20:13:52 -0300

    correction2

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
-import searchAlbumsAPI from '../services/userAPI';
+// import searchAlbumsAPI from '../services/userAPI';
 import Header from './Header';
 
 class Search extends React.Component {
-  constructor() {
-    super();
+  // constructor() {
+  //   super();
 
-    this.state = {
-      artName: '',
-      loadState: false,
-      currAlbums: [],
-      artistKept: '',
-      albumsExists: false,
-    };
-  }
+  //   this.state = {
+  //     artName: '',
+  //     loadState: false,
+  //     currAlbums: [],
+  //     artistKept: '',
+  //     albumsExists: false,
+  //   };
+  // }
 
-  change = ({ target: { value } }) => {
-    this.setState({ artName: value });
-  };
+  // change = ({ target: { value } }) => {
+  //   this.setState({ artName: value });
+  // };
 
-  searchButton = async () => {
-    const { artName } = this.state;
-    const albumsFetch = await searchAlbumsAPI(artName);
-    this.setState({ loadState: true },
-      albumsFetch()
-        .then(() => this.setState({
-          loadState: false,
-          artName: '',
-          artistKept: artName,
-          currAlbums: albumsFetch,
-          albumsExists: true,
-        })));
-  }
+  // searchButton = async () => {
+  //   const { artName } = this.state;
+  //   const albumsFetch = await searchAlbumsAPI(artName);
+  //   this.setState({ loadState: true },
+  //     albumsFetch()
+  //       .then(() => this.setState({
+  //         loadState: false,
+  //         artName: '',
+  //         artistKept: artName,
+  //         currAlbums: albumsFetch,
+  //         albumsExists: true,
+  //       })));
+  // }
 
   render() {
     return (
